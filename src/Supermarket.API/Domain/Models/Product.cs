6e3a45b098052d277a5afd814c21e7b6c8cfc125
WahibAshraf <Wahib1900@gmail.com>
2020-03-13 20:52:33 +0800

    Map EF relationship using fluent API

--- src/Supermarket.API/Domain/Models/Product.cs
+++ src/Supermarket.API/Domain/Models/Product.cs
@@ -4,6 +4,7 @@ namespace Supermarket.API.Domain.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public short QuantityInPackage { get; set; }
         public EUnitOfMeasurement UnitOfMeasurement { get; set; }
 
         // public int CategoryId { get; set; }
--- src/Supermarket.API/Persistance/Contexts/AppDbContext.cs
+++ src/Supermarket.API/Persistance/Contexts/AppDbContext.cs
@@ -13,6 +13,29 @@ namespace Supermarket.API.Persistance.Contexts
        {
              base.OnModelCreating(modelBuilder);
              
+            /* Category Table Configuration through fluent API */
+             modelBuilder.Entity<Category>().ToTable("Categories");
+             modelBuilder.Entity<Category>().HasKey(x => x.Id);
+
+             modelBuilder.Entity<Category>().Property(x => x.Id).IsRequired().ValueGeneratedOnAdd();
+             modelBuilder.Entity<Category>().Property(x => x.Name).IsRequired().HasMaxLength(30);
+
+
+             modelBuilder.Entity<Category>().HasData(
+                  new Category{ Id = -1, Name = "Fruits and Vegetables"}, new Category { Id = -2, Name = "Diary"}
+                  );
+
+
+
+
+            /* Product Table Configuration through fluent API */
+            modelBuilder.Entity<Product>().ToTable("Products");
+            modelBuilder.Entity<Product>().HasKey(x => x.Id);
+
+            modelBuilder.Entity<Product>().Property(x =>x.Id).IsRequired().ValueGeneratedOnAdd();
+            modelBuilder.Entity<Product>().Property(x =>x.Name).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<Product>().Property(x => x.QuantityInPackage).IsRequired();
+            modelBuilder.Entity<Product>().Property(x => x.UnitOfMeasurement).IsRequired();
        }
     }
 }
