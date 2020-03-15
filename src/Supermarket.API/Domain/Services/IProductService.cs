dc669d0f185318bbd75015757c4ff9c6dec905a9
WahibAshraf <Wahib1900@gmail.com>
2020-03-15 22:13:53 +0800

    Completed the Service and repository of the Product

--- src/Supermarket.API/Domain/Services/IProductService.cs
+++ src/Supermarket.API/Domain/Services/IProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Services.Communication;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,8 @@ namespace Supermarket.API.Domain.Services
     public interface IProductService
     {
         Task<IEnumerable<Product>> ListAsync();
-        Task<ProductResponse> SaveAsync();
-        Task<ProductResponse> UpdateAsync();
-        Task<ProductResponse> DeleteAsync();
+        Task<ProductResponse> SaveAsync(Product product);
+        Task<ProductResponse> UpdateAsync(int id, Product product);
+        Task<ProductResponse> DeleteAsync(int id);
     }
 }
---
+++ src/Supermarket.API/Persistance/Repositories/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Repositories;
+using Supermarket.API.Persistance.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Persistance.Repositories
+{
+    public class ProductRepository : BaseRepository, IProductRepository
+    {
+        public ProductRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+
+        public async Task<IEnumerable<Product>> ListAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+        }
+        public async Task<Product> FindByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+        }
+        public void Remove(Product product)
+        {
+            _context.Products.Remove(product);
+        }
+    }
+}
